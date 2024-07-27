2ea18b4519fa50005e3d48b2361068348719566f
Sinan Aslan <101702843+sinanaslaan@users.noreply.github.com>
2024-07-27 06:16:36 +0300

    services and list added

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const loadCountryApi = () => {
+  const apiUrl = "https://restcountries.com/v3.1/all";
+
+  fetch(apiUrl)
+  .then(res => res.json())
+  .then(data => displayCountries(data))
+};
+
+const displayCountries = countries => {
+  const countriesHTML = countries.map(country => getCountry(country))
+  const container = document.getElementById('country-item');
+  container.innerHTML = countriesHTML.join('');
+};
+
+const getCountry = (country) => {
+  return`
+    <div class="country__item">
+      <img src="${country.flags.png}" alt="${country.name.common}">
+      <div class="country__info">
+        <div class="country__info-item">
+          <h2>${country.name.common}</h2>
+        </div>
+        <div class="country__info-item">
+          <h3>Population:</h3>
+          <span>${country.population}</span>
+        </div>
+        <div class="country__info-item">
+          <h3>Region:</h3>
+          <span>${country.region}</span>
+        </div>
+        <div class="country__info-item">
+          <h3>Capital:</h3>
+          <span>${country.capital}</span>
+        </div>
+      </div>
+    </div>
+  `;
+};
+
+
+loadCountryApi();
